8dc7a78ef3f137edf83593cd8554f16fc2b9efd0
Donald McKendrick <ddmckendrick@gmail.com>
2014-08-13 00:47:05 +0100

    gulp static server and rev

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
+var connect = require('gulp-connect');
  
 gulp.task('sass', function() {
   return gulp.src('src/scss/app.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('scripts', function() {
@@ -18,9 +21,28 @@ gulp.task('scripts', function() {
         }))
         .pipe(uglify())
         .pipe(gulp.dest('./build/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('rev', ['sass', 'scripts'], function() {
+  return gulp.src(['build/**/*.css', 'build/**/*.js'])
+    .pipe(rev())
+    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['sass', 'scripts'], function() {
   gulp.watch('src/scss/**/*.scss', ['sass']);
   gulp.watch('src/js/**/*.js', ['scripts']);
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true,
+    port: 8000
+  });
+});
+
+gulp.task('default', ['connect', 'watch']);
