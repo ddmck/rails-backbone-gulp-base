9a33d585433cc44e254ba513d689bd657339c512
Donald McKendrick <ddmckendrick@gmail.com>
2014-08-13 23:52:42 +0100

    Add backbone, underscore, eco and jQuery

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,3 +1,5 @@
-var main = require('./modules/main.js')
-
-main()
+var _ = require('underscore');
+var $ = require('jquery')(window);
+var Backbone = require('backbone');
+Backbone.$ = $;
+var main = require('./modules/main.js');
