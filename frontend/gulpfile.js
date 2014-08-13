259862fabb22d1d69be8e19b46ba0233fc5d3b63
Donald McKendrick <ddmckendrick@gmail.com>
2014-08-13 23:21:13 +0100

    Add autoprefixer

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -4,10 +4,12 @@ var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var rev = require('gulp-rev');
 var connect = require('gulp-connect');
- 
+var autoprefixer = require('gulp-autoprefixer');
+
 gulp.task('sass', function() {
   return gulp.src('src/scss/app.scss')
     .pipe(sass())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(gulp.dest('./build/css'))
     .pipe(connect.reload());
 });
