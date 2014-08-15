662fdce2bda8c244b9e78a364532980de0fdd9df
Donald McKendrick <ddmckendrick@gmail.com>
2014-08-15 21:31:01 +0100

    Un-name all the things

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Barklist_session'
+Rails.application.config.session_store :cookie_store, key: '_App_session'
